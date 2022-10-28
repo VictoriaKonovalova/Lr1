d14238ddaae6adde7922facf9e919b42029939f2
VictoriaKonovalova <112749149+VictoriaKonovalova@users.noreply.github.com>
2022-10-28 17:06:13 +0300

    Commit_2

--- ConsoleApplication_Lr1/ConsoleApplication_Lr1.cpp
+++ ConsoleApplication_Lr1/ConsoleApplication_Lr1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main() {
 	int a[1024];
-	for (int i = 0; i <= 1024; i++)
+	for (int i = 0; i <= 40; i++)
 		a[i] = i;
-	for (int i = 0; i <= 1024; i++)
+	for (int i = 0; i <= 40; i++)
 		cout << a[i] << ' ';
 }

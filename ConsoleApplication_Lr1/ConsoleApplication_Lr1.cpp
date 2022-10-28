5673ca3722aad99ed1dc58e437ff55fa22a3de49
VictoriaKonovalova <112749149+VictoriaKonovalova@users.noreply.github.com>
2022-10-28 19:28:04 +0300

    Commit_6

--- ConsoleApplication_Lr1/ConsoleApplication_Lr1.cpp
+++ ConsoleApplication_Lr1/ConsoleApplication_Lr1.cpp
@@ -1,16 +1,12 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
-	/* Объявление двумерного массива*/
 	const int N = 2, M = 3;
 	int A[N][M];
-	
 	int i, j;
 	for (i = 0; i < N; i++)
 		for (j = 0; j < M; j++)
 			cin >> A[i][j];
-
-	
 	for (i = 0; i < N; i++) {  
 		for (j = 0; j < M; j++)  
 			cout << A[i][j] << " ";
